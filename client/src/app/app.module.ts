24f5853d566bf4b57d10ee560a46e8487a8659f6
soullaft <soullaft@gmail.com>
2022-04-15 10:31:20 +0300

    added home component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
