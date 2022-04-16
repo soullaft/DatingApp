20bcf49fdd66fe02c5f89c876e80d9bfd74a663b
soullaft <soullaft@gmail.com>
2022-04-16 05:09:16 +0300

    updated home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  registerMode: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
 }
