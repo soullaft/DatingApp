b2c6e8942256c45e09090b6305137e894e84bdaf
soullaft <soullaft@gmail.com>
2022-07-25 22:07:46 +0300

    configured automapper for PhotoUrl in MemberDto

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -8,7 +8,11 @@ namespace API.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<AppUser, MemberDto>();
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt
+                .MapFrom(src => src.Photos
+                .FirstOrDefault(x => x.IsMain).Url));
+
             CreateMap<Photo, PhotoDto>();
         }
     }
