a9e5a2ada25ae5fa7cf9e503ff02907523a34902
soullaft <soullaft@gmail.com>
2022-07-28 23:44:26 +0300

    added some more styling in member-card

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
 @Component({
