266dd55c9abd8b97f27626c142d894c48f53b3b7
soullaft <soullaft@gmail.com>
2022-08-11 02:22:09 +0300

    added remove photo client

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -45,6 +45,12 @@ export class PhotoEditorComponent implements OnInit {
     })
   }
 
+  deletePhoto(photo: Photo) {
+    this.memberService.deletePhoto(photo.id).subscribe(() => {
+      this.member.photos = this.member.photos.filter(p => p.id !== photo.id)
+    })
+  }
+
   initializeUploader() {
     this.uploader = new FileUploader({
       url: this.baseUrl + 'Users/add-photo',
