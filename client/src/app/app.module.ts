9a53ff1821ef28167dc75f76ac97d00dcd9c308e
soullaft <soullaft@gmail.com>
2022-08-12 23:08:38 +0300

    added validators to register

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +13,6 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
@@ -50,6 +48,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     NgxSpinnerModule
   ],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from 'src/app/_services/account.service';
 
@@ -9,26 +10,36 @@ import { AccountService } from 'src/app/_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private accountService: AccountService,
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.minLength(4), Validators.maxLength(12), Validators.required]),
+      confirmPassword: new FormControl('', Validators.required)
+    })
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: resp => {
-        console.log(resp);
-        this.cancel()
-      },
-      error: e => {
-        console.log(e);
-        this.toastr.error(e); 
-      }
-    });
+    console.log(this.registerForm.value);
+    // // this.accountService.register(this.model).subscribe({
+    // //   next: resp => {
+    // //     console.log(resp);
+    // //     this.cancel()
+    // //   },
+    // //   error: e => {
+    // //     console.log(e);
+    // //     this.toastr.error(e); 
+    // //   }
+    // // });
   }
 
   cancel() {
