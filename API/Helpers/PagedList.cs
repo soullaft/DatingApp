5148a608dc91f77aec796f65237e5ac871e28f35
soullaft <soullaft@gmail.com>
2022-08-15 07:16:03 +0300

    added comments

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -12,6 +12,13 @@ namespace API.Helpers
 
         public int TotalCount { get; set; }
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="count">count</param>
+        /// <param name="pageNumber">page number</param>
+        /// <param name="pageSize">page size</param>
         public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
         {
             CurrentPage = pageNumber;
@@ -21,6 +28,14 @@ namespace API.Helpers
             AddRange(items);
         }
 
+        /// <summary>
+        /// Execute and pagged <see cref="IQueryable{T}"/> expression
+        /// with skip/take results of what we need
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="pageNumber">number of page</param>
+        /// <param name="pageSize">page size</param>
+        /// <returns></returns>
         public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
         {
             var count = await source.CountAsync();
