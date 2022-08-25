06b35cfcbd220200a9f9510729cb2167eec23ad1
soullaft <soullaft@gmail.com>
2022-08-26 02:22:27 +0300

    added gender in dto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -54,7 +54,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -86,7 +87,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
