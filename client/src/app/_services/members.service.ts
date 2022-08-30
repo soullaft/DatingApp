d6a5874d01ed561275d64733c9570c797a4d96a8
soullaft <soullaft@gmail.com>
2022-08-30 03:24:51 +0300

    cache user

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, of } from 'rxjs';
+import { map, of, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { PaginatedResult } from '../_models/pagination';
+import { User } from '../_models/user';
 import { UserParams } from '../_models/userParams';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,28 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
   memberCache = new Map();
+  user: User;
+  userParams: UserParams;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+      this.userParams = new UserParams(user);
+    })
+   }
+
+   getUserParams() {
+    return this.userParams;
+   }
+
+   setUserParams(params: UserParams) {
+    this.userParams = params;
+   }
+
+   resetUserParams(){
+    this.userParams = new UserParams(this.user);
+    return this.userParams;
+   }
 
   getMembers(userParams: UserParams) {
     var response = this.memberCache.get(Object.values(userParams).join('-'));
@@ -37,8 +59,11 @@ export class MembersService {
   }
 
   getMember(userName: string) {
-    const member = this.members.find(m => m.userName === userName);
-    if (member !== undefined) return of(member);
+    const member = [...this.memberCache.values()].reduce((arr, elem) => arr.concat(elem.result), []).find((member: Member) => member.userName === userName);
+
+    if(member) {
+      return of(member);
+    }
     return this.http.get<Member>(this.baseUrl + 'users/' + userName);
   }
 
