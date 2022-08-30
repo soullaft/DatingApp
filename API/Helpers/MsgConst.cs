4509c1b8f71ca61541eec9caed7ea0e6683db2e3
soullaft <soullaft@gmail.com>
2022-08-30 16:20:54 +0300

    added LikesController

--- API/Helpers/MsgConst.cs
+++ API/Helpers/MsgConst.cs
@@ -3,6 +3,6 @@
     public static class MsgConst
     {
         public const string LIKE_PREDICATE = "liked";
-        public const string LIKED_BY_PREDICATE = "liked";
+        public const string LIKED_BY_PREDICATE = "likedBy";
     }
 }
