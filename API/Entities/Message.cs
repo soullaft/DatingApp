750785877803c817c96432376dda8e3ef6127418
soullaft <soullaft@gmail.com>
2022-08-31 19:14:56 +0300

    added message entity

---
+++ API/Entities/Message.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public int SenderId { get; set; }
+
+        public string? SenderUsername { get; set; }
+
+        public AppUser? Sender { get; set; }
+
+        public int RecipientId { get; set; }
+
+        public int RecipientUsername { get; set; }
+
+        public AppUser? Recipient { get; set; }
+    }
+}
