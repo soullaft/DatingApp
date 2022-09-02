5e4a61cbafe6d05d39d69d6d9fdd3fad432b012b
soullaft <soullaft@gmail.com>
2022-09-02 03:49:19 +0300

    added comments to LogUserActivity

--- API/Filters/LogUserActivity.cs
+++ API/Filters/LogUserActivity.cs
@@ -1,19 +1,29 @@
-﻿using API.Extensions;
+﻿using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace API.Filters
 {
+    /// <summary>
+    /// Log user activity after method of controller has been fired
+    /// Update <see cref="AppUser"/> LastActive
+    /// </summary>
     public class LogUserActivity : IAsyncActionFilter
     {
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
+            //wait while method of controller has been invoked
             var resultContext = await next();
 
+            //if user is not authenticated exit this method
             if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
 
             var userId = resultContext.HttpContext.User.GetUserId();
             var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
+
+            if(repo == null) throw new NullReferenceException(nameof(IUserRepository));
+
             var user = await repo.GetByIdAsync(userId);
             user.LastActive = DateTime.Now;
             await repo.SaveAllAsync();
