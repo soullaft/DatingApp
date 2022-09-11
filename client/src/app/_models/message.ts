239b5c45389bcf7f5948d688f4e5b2b9906b8c6f
soullaft <soullaft@gmail.com>
2022-09-11 18:37:45 +0300

    added Message intefrace

---
+++ client/src/app/_models/message.ts
@@ -0,0 +1,12 @@
+interface Message {
+    id: number;
+    senderId: number;
+    senderUsername: string;
+    senderPhotoUrl: string;
+    recipientId: number;
+    recipientUsername: string;
+    recipientPhotoUrl: string;
+    content: string;
+    dateRead?: any;
+    messageSend: string;
+}
