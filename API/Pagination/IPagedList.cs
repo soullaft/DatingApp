943445eb7dab8d74de0751720444c6392f9178ea
soullaft <soullaft@gmail.com>
2022-09-13 22:22:09 +0300

    added comments

--- API/Pagination/IPagedList.cs
+++ API/Pagination/IPagedList.cs
@@ -5,12 +5,24 @@
     /// </summary>
     public interface IPagedList
     {
+        /// <summary>
+        /// Current page number
+        /// </summary>
         public int CurrentPage { get; set; }
 
+        /// <summary>
+        /// Total pages count
+        /// </summary>
         public int TotalPages { get; set; }
 
+        /// <summary>
+        /// Page size (elements per on page)
+        /// </summary>
         public int PageSize { get; set; }
 
+        /// <summary>
+        /// Total count of elements
+        /// </summary>
         public int TotalCount { get; set; }
     }
 }
--- API/Pagination/PaginationHeader.cs
+++ API/Pagination/PaginationHeader.cs
@@ -4,12 +4,24 @@ namespace API.Pagination
 {
     public sealed class PaginationHeader : IPaginationHeader
     {
+        /// <summary>
+        /// Current page number
+        /// </summary>
         public int CurrentPage { get; set; }
 
+        /// <summary>
+        /// Items per one page
+        /// </summary>
         public int ItemsPerPage { get; set; }
 
+        /// <summary>
+        /// Total count of items
+        /// </summary>
         public int TotalItems { get; set; }
 
+        /// <summary>
+        /// Total pages count
+        /// </summary>
         public int TotalPages { get; set; }
     }
 }
